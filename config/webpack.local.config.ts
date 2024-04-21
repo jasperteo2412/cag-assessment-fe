0c52a38a5f18e3999777939de7f3be71c963f85a
Jasper Teo <43600109+jasperteo2412@users.noreply.github.com>
2024-04-21 18:12:49 +0800

    Updated webpack config files

--- config/webpack.local.config.ts
+++ config/webpack.local.config.ts
@@ -1,7 +1,7 @@
 import { merge } from 'webpack-merge';
-import Dotenv from 'dotenv-webpack';
 
 const config = require('./webpack.config');
+const Dotenv = require('dotenv-webpack');
 const devConfig = {
     mode: 'development',
     plugins: [
